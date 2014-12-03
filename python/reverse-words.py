124c8c6fd44aa6a01fa448676badb6c0e44e76ca
chetankhilosiya <chetan.khilosiya@gmail.com>
2014-12-03 10:28:58 +0530

    The reverse words code for python is added.

--- python/reverse-words.py
+++ python/reverse-words.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-test_cases  = int(raw_input())
+test_cases  = int(input())
 i=0
 while i < test_cases:
-	line = raw_input()
+	line = input()
 	array = line.split()
-	print "Case #%d"%(i+1)+":",
-	#for word in array[::-1]:
-	print " ".join(array[::-1]),
-	#print array[0]
+	print("Case #%d"%(i+1)+":",end="")
+	for word in array[::-1]:
+		print(" "+word,end="")
+	if i != test_cases-1:
+		print()
 	i = i+1
